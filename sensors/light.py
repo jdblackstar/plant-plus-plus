88813d8e6aa8e333aa522db03b46bd3ff5654071
Josh Black-Star <josh@blackstar.dev>
2023-08-15 16:16:54 -0700

    add light sensor classes

---
+++ sensors/light.py
@@ -0,0 +1,30 @@
+from smbus2 import SMBus
+
+
+class LightSensor:
+    """Abstract base class for light sensors."""
+
+    def read_foot_candles(self):
+        raise NotImplementedError("This method should be overridden by a subclass")
+
+
+class BH1750Sensor(LightSensor):
+    """Class for the BH1750 light sensor."""
+
+    ADDRESS = 0x23
+    COMMAND = 0x10
+
+    def read_foot_candles(self):
+        with SMBus(1) as bus:
+            lux_data = bus.read_i2c_block_data(self.ADDRESS, self.COMMAND, 2)
+            lux = (lux_data[0] << 8 | lux_data[1]) / 1.2
+            return lux * 0.0929
+
+
+class TSL2561Sensor(LightSensor):
+    """Class for the TSL2561 light sensor."""
+
+    # We aren't implementing this sensor yet, so nothing to put here
+
+    def read_foot_candles(self):
+        pass
