a87ddf2a58b9029397a83fb609d7708a30047ca5
Josh Black-Star <josh@blackstar.dev>
2023-08-19 10:22:51 -0700

    add docstrings and change how to handle the sleep

--- controllers/led.py
+++ controllers/led.py
@@ -6,6 +6,13 @@ import neopixel
 
 class LED:
     def __init__(self, pin, num_leds):
+        """
+        Initialize the LED strip with the given parameters.
+
+        Args:
+            pin (board pin): Pin to which the LED strip is connected.
+            num_leds (int): Number of LEDs in the strip.
+        """
         self.pin = pin
         self.num_leds = num_leds
 
@@ -21,22 +28,64 @@ class LED:
 
 class NeoPixelLED(LED):
     def __init__(self, pin, num_leds):
+        """
+        Initialize the NeoPixel LED strip with the given parameters.
+
+        Args:
+            pin (board pin): Pin to which the LED strip is connected.
+            num_leds (int): Number of LEDs in the strip.
+        """
         super().__init__(pin, num_leds)
         self.pixels = neopixel.NeoPixel(pin, num_leds)
 
-    def turn_on(self):
+    def turn_on(self, color=(255, 255, 255)):
+        """
+        Turn on the LED strip.
+
+        Args:
+            color (tuple): RGB color tuple.
+        """
         for i in range(self.num_leds):
-            self.pixels[i] = (255, 255, 255)  # White color
+            self.pixels[i] = color  # White color
+
+        # could also just do this, I think - gotta test it
+        # self.pixels.fill((255, 255, 255))
 
     def turn_off(self):
+        """
+        Turn off the LED strip.
+
+        Args:
+            color (tuple): RGB color tuple.
+        """
         for i in range(self.num_leds):
             self.pixels[i] = (0, 0, 0)  # Off
 
+        # could also just do this, I think - gotta test it
+        # self.pixels.fill((0, 0, 0))
+
     def set_color(self, color):
+        """
+        Set the color of the LED strip.
+
+        Args:
+            color (tuple): RGB color tuple.
+        """
         for i in range(self.num_leds):
             self.pixels[i] = color
 
+        # could also just do this, I think - gotta test it
+        # self.pixels.fill(color)
+
     def transition_color(self, start_color, end_color, duration):
+        """
+        Transition the color of the LED strip from start_color to end_color over the given duration.
+
+        Args:
+            start_color (tuple): RGB color tuple.
+            end_color (tuple): RGB color tuple.
+            duration (float): Duration of the transition in seconds.
+        """
         steps = 100
         delay = duration / steps
 
@@ -48,5 +97,9 @@ class NeoPixelLED(LED):
                 int(start_color[2] + (end_color[2] - start_color[2]) * ratio),
             )
 
+            start_time = time.time()
             self.set_color(color)
-            time.sleep(delay)
+            elapsed_time = time.time() - start_time
+
+            adjusted_delay = max(0, delay - elapsed_time)
+            time.sleep(adjusted_delay)
