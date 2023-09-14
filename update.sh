326cbe2f14a4954a7e8b6fb69a88fc0a58deca42
Josh Black-Star <23347017+jdblackstar@users.noreply.github.com>
2023-09-13 22:39:00 -0700

    it's late and i'm dumb

--- update.sh
+++ update.sh
@@ -4,13 +4,13 @@
 source .env
 
 # List of Raspberry Pi IP addresses
+# RASPBERRY_PI_IPS is a space-separated string, like this:
+# RASPBERRY_PI_IPS = "192.168.1.10 192.168.1.11 192.168.1.12"
 IFS=' ' read -r -a RASPBERRY_PI_IPS <<< "$RASPBERRY_PI_IPS"
 
-# Make sure that your .env file contains both PROJECT_DIR and DEST_DIR
-
-# PROJECT_DIR is a space-separated string, like this:
-# RASPBERRY_PIS="192.168.1.10 192.168.1.11 192.168.1.12"
-
+# Make sure that your .env file also contains both PROJECT_DIR and DEST_DIR
+# PROJECT_DIR is the code on your machine
+# DEST_DIR is the directory where the code lives on your raspberry pi
 # DEST_DIR will likely just be "/home/pi"
 
 for ip in "${RASPBERRY_PI_IPS[@]}"; do
