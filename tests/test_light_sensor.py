3b9be68816f5f8a073b2f405573bb11e9662fcb6
Josh Black-Star <23347017+jdblackstar@users.noreply.github.com>
2023-10-27 17:04:52 -0700

    add quick light sensor test

---
+++ tests/test_light_sensor.py
@@ -0,0 +1,23 @@
+from sensors.light import BH1750Sensor
+
+def test_bh1750():
+    sensor = BH1750Sensor()
+
+    print("Initializing the sensor...")
+    sensor.initialize()
+
+    print("Powering on the sensor...")
+    sensor.power_on()
+
+    print("Reading lux value...")
+    lux = sensor.read_lux()
+    print(f"Lux value: {lux}")
+
+    print("Powering off the sensor...")
+    sensor.power_off()
+
+def main():
+    test_bh1750()
+
+if __name__=="__main__":
+    main()
